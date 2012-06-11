52a8554b203b63076a1671d804729d5de439f89f
Caike Souza <carloshrsouza@gmail.com>
2012-06-11 00:42:26 -0400

    Listando recentes no sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_recent
+
+  def load_recent
+    @recent_talks = Talk.order('created_at DESC').limit(3)
+  end
 end
